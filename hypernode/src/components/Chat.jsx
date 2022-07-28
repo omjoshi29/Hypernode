f9c1a182a8159a250eb4e018c0299f9bc6444bdf
nimishshinde <mayurshinde2310@gmail.com>
2022-07-29 02:08:16 +0530

    updates

--- hypernode/src/components/Chat.jsx
+++ hypernode/src/components/Chat.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
 import { io } from "socket.io-client";
+import "./chat.css"
 
 export const Chat = () => {
   let [message, setmessage] = useState([]);
   let [send, setSend] = useState("");
+  let [show,setshow] = useState(true)
   let socket = io("http://localhost:8080");
   socket.on("message", (msg) => {
     setmessage([...message, msg]);
@@ -19,35 +21,40 @@ export const Chat = () => {
     socket.emit("message", send);
   };
 
-  return (
-    <div>
-      <input
-        type={"text"}
-        placeholder="enter your msg"
-        onChange={(e) => setSend(e.target.value)}
-      />
-      <button onClick={handleclick}>send</button>
+  return <>
+  <div> <button>show</button>
+  <button>hide</button></div>
+    <div className="chatbox">
+      <div className="chatwindow">
       {message.map((el, index) => {
         return (
           <div>
-            {el.client && <p> {el.client} : user</p>}
-            <p>bot : {el.server}</p>
+            {el.client && <p className="user"> {el.client} : user</p>}
+            <div  className="bot">
+            <p className="ser">bot : {el.server}</p>
             {el.choice &&
               el.choice.map((el) => {
                 return (
                   <div>
                     {" "}
-                    {!el.subcats && <p>{el.name}</p>}
+                     <p>{el.name}</p>
                     {el.subcats &&
                       el.subcats.map((el) => {
                         return <p>{el.name}</p>;
                       })}
                   </div>
                 );
-              })}
+              })}</div>
           </div>
         );
-      })}
-    </div>
-  );
+      })}</div>
+      <div className="chatip">
+      <input
+        type={"text"}
+        placeholder="enter your msg"
+        onChange={(e) => setSend(e.target.value)}
+      />
+      <button onClick={handleclick}>send</button>
+    </div></div>
+  </>
 };
