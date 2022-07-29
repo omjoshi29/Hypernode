7810e8ad40b2fa03baa85db5bc7f933791d11de8
nimishshinde <mayurshinde2310@gmail.com>
2022-07-29 20:23:19 +0530

    update

--- hypernode/src/App.js
+++ hypernode/src/App.js
@@ -1,26 +1,16 @@
-<<<<<<< Updated upstream
 import "./App.css";
 import { Chat } from "./components/Chat";
+import { Footer } from "./components/Footer";
 import { Home } from "./components/Home";
 import { Navbar } from "./components/Navbar";
 import Mainroutes from "./Mainroutes";
-=======
-import './App.css';
-import { Footer } from './components/Footer';
-import { Home } from './components/Home';
-import { Navbar } from './components/Navbar';
->>>>>>> Stashed changes
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-<<<<<<< Updated upstream
       <Mainroutes />
-=======
-      <Home/>
       <Footer/>
->>>>>>> Stashed changes
     </div>
   );
 }
